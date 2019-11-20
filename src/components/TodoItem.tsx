0c629d680dc533d4e10809a90a23f33c4d7a30c1
antnzhlnkn <antnzhlnkn@gmail.com>
2019-11-20 18:00:19 +0300

    add styles

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -9,6 +9,9 @@ import Checkbox from '@material-ui/core/Checkbox';
 import {Input} from "@material-ui/core";
 import Box from "@material-ui/core/Box";
 import moment from "moment";
+import Button from "@material-ui/core/Button";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+
 
 interface IIodoItemProps {
     item: any;
@@ -55,7 +58,7 @@ class TodoItem extends Component<IIodoItemProps, IIodoItemState> {
     render() {
         const {isEdit} = this.state;
         return (
-            <Box mb={1}>
+            <Box display="flex" flexDirection="column" mb={1}>
                 <Card>
                     <CardContent>
                         {
@@ -73,13 +76,19 @@ class TodoItem extends Component<IIodoItemProps, IIodoItemState> {
                                 )
 
                         }
-                        <Checkbox
-                            disabled={!this.state.isEdit}
-                            name="isDone"
-                            checked={this.state.isDone}
-                            onChange={this.handleCheck}
-                        >
-                        </Checkbox>
+                        <div>
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    disabled={!this.state.isEdit}
+                                    name="isDone"
+                                    checked={this.state.isDone}
+                                    onChange={this.handleCheck}
+                                />
+                            }
+                            label="Done"
+                            />
+                        </div>
                         <div>
                             {this.state.date ?
                                 <span>Hours: {((moment.duration(moment().unix() * 1000).asHours()) - moment.duration(this.state.date.seconds * 1000).asHours()).toFixed(1)}  </span> : null}
@@ -91,14 +100,14 @@ class TodoItem extends Component<IIodoItemProps, IIodoItemState> {
                         {
                             isEdit ? (
                                     <>
-                                        <button onClick={this.handleSave}><SaveIcon>Save</SaveIcon></button>
-                                        <button><DeleteIcon>Delete item</DeleteIcon></button>
+                                        <Button onClick={this.handleSave}><SaveIcon>Save</SaveIcon></Button>
+                                        <Button><DeleteIcon>Delete item</DeleteIcon></Button>
                                     </>
                                 )
                                 :
                                 (
 
-                                    <button onClick={this.handleEdit}><EditIcon>Edit</EditIcon></button>
+                                    <Button onClick={this.handleEdit}><EditIcon>Edit</EditIcon></Button>
                                 )
                         }
                     </CardActions>
