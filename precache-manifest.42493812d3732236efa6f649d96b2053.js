3f5b972ea3623ab90a1c43dad8a444a3b156a475
antnzhlnkn <39551170+antnzhlnkn@users.noreply.github.com>
2020-01-09 13:50:58 +0000

    Deploying to gh-pages from master fff1deb10d629c3f98af899f380514c9bf417746

---
+++ precache-manifest.42493812d3732236efa6f649d96b2053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61019b100431268470400b282fe2febb",
+    "url": "/tstemplate/index.html"
+  },
+  {
+    "revision": "9e4d121c6bb749d13c6e",
+    "url": "/tstemplate/static/css/main.896a4009.chunk.css"
+  },
+  {
+    "revision": "d999580edf11fa457177",
+    "url": "/tstemplate/static/js/2.ecc689ff.chunk.js"
+  },
+  {
+    "revision": "91c2b35cdad13e08be501c3b1250c2e7",
+    "url": "/tstemplate/static/js/2.ecc689ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e4d121c6bb749d13c6e",
+    "url": "/tstemplate/static/js/main.cf0ad2f3.chunk.js"
+  },
+  {
+    "revision": "e3bceb354fce69799881",
+    "url": "/tstemplate/static/js/runtime-main.117450b0.js"
+  }
+]);
